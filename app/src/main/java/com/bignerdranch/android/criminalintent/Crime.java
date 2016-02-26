4f974e9d1c20a102f564667d1ad4a50d519583c4
Peter Schwartz <pschwart@gustavus.edu>
2016-02-26 12:12:51 -0600

    Criminal Intent Project

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,43 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by peterschwartz on 2/23/16.
+ */
+public class Crime {
+    private UUID mID;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mID = UUID.randomUUID();
+        mDate = new Date();
+    }
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+    public boolean isSolved() {
+        return mSolved;
+    }
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
