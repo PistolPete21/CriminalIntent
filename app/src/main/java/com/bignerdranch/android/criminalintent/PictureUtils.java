0552376fdc33975a24376066321803f4480a14b4
Peter Schwartz <pschwart@gustavus.edu>
2016-04-03 22:47:54 -0500

    Challenge Chp 16: Detail Display

--- app/src/main/java/com/bignerdranch/android/criminalintent/PictureUtils.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/PictureUtils.java
@@ -3,22 +3,27 @@ package com.bignerdranch.android.criminalintent;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.Point;
+import android.media.ExifInterface;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 /**
  * Created by peterschwartz on 4/2/16.
  */
 public class PictureUtils {
-
     public static Bitmap getScaledBitmap(String path, Activity activity) {
         Point size = new Point();
-        activity.getWindowManager().getDefaultDisplay().getSize(size);
+        activity.getWindowManager().getDefaultDisplay()
+                .getSize(size);
 
         return getScaledBitmap(path, size.x, size.y);
     }
 
     public static Bitmap getScaledBitmap(String path, int destWidth, int destHeight) {
-        //Read in the dimensions of the image on disk
+        // read in the dimensions of the image on disk
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(path, options);
@@ -26,7 +31,6 @@ public class PictureUtils {
         float srcWidth = options.outWidth;
         float srcHeight = options.outHeight;
 
-        //Figure out how much to scale down by
         int inSampleSize = 1;
         if (srcHeight > destHeight || srcWidth > destWidth) {
             if (srcWidth > srcHeight) {
@@ -39,7 +43,6 @@ public class PictureUtils {
         options = new BitmapFactory.Options();
         options.inSampleSize = inSampleSize;
 
-        //Read in and create final bitmap
         return BitmapFactory.decodeFile(path, options);
     }
 }
